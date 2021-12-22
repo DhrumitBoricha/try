ab5c5343de272f481598b4ecc8a15ac699f1a6ad
DhrumitBoricha <82996982+DhrumitBoricha@users.noreply.github.com>
2021-12-22 22:04:54 +0530

    Add files via upload

---
+++ assets/js/logo.js
@@ -0,0 +1,13 @@
+<script>
+	function before(){
+		document.getElementById('myImage')
+		.src="assets/img/home.png";
+		
+	}
+	
+	function afterr(){
+		document.getElementById('myImage')
+		.src="assets/img/l.png";
+		
+	}
+</script>
